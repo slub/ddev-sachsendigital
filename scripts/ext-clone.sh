53433f75d19b3e9a86567d7a16931f0aebd73109
Kajetan Dvoracek <kajetan.dvoracek@slub-dresden.de>
2021-09-09 09:43:33 +0200

    Depend on slub/slub-digitalcollections from main package

--- scripts/ext-clone.sh
+++ scripts/ext-clone.sh
@@ -11,4 +11,5 @@ set -x
 mkdir -p "$EXTDIR"
 cd "$EXTDIR"
 git clone git@github.com:kitodo/kitodo-presentation.git
+git clone git@github.com:slub/slub_digitalcollections.git
 git clone git@git.slub-dresden.de:slub-webseite/slub-web-ldp.git
