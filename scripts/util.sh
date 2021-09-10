b0a1ca8406b02b549be8954f8c84c4c1634d7ef5
Kajetan Dvoracek <kajetan.dvoracek@slub-dresden.de>
2021-09-10 17:10:30 +0200

    Move db.sql to new data/ subdirectory
    This is to prepare adding further data to the repo, such as metadata
    records and lists of document URLs.

--- scripts/util.sh
+++ scripts/util.sh
@@ -4,7 +4,7 @@ function db_file()
 {
     if [ -z $1 ]; then
         SCRIPTDIR=$(dirname "$0")
-        DBFILE="$SCRIPTDIR/../db.sql"
+        DBFILE="$SCRIPTDIR/../data/db.sql"
     else
         DBFILE=$1
     fi
